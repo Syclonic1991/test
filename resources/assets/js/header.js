e7c87220ea35582ad66052c3cf8cf00000751454
Yevhen Voichenko <yevhen.voichenko@abz.agency>
2018-03-12 12:47:37 +0300

    update header effect

--- resources/assets/js/header.js
+++ resources/assets/js/header.js
@@ -6,23 +6,34 @@ $(function () {
   const $cloneBody = $($clone[0].contentDocument).find('body');
   const $cloneHead = $($clone[0].contentDocument).find('head');
   const $deferredStyles = $('#deferred-styles');
-  $('.bannerContainer').on('loaded', function () {
+
+  $('.bannerContainer').on('loading', () => {
+    $blurWrap.css({
+      'transition': 'none',
+      'opacity': '0',
+    });
+  });
+
+  $('.bannerContainer').on('loaded', () => {
     console.log('images loaded');
     $cloneBody.html($content.html());
     $cloneBody.append($deferredStyles.html());
     $cloneHead.html($head.html());
     $cloneBody.css({
       'overflow': 'hidden',
-      'transform': `translateY(-${window.scrollY}px)`,
+      'transform': `translate3d(0, -${window.scrollY}px, 0)`,
       'padding-top': '20px',
       'will-change': 'transform',
     });
   setTimeout(() => {
-    $blurWrap.css('opacity', 1);
+    $blurWrap.css({
+      'transition': '',
+      'opacity': '1',
+    });
   }, 300);
   });
 
-  $(window).on('scroll', function () {
+  $(window).on('scroll', () => {
     $cloneBody.css('transform', `translate3d(0, -${window.scrollY}px, 0)`);
   });
 });
